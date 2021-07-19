4fd4f1dfb3a60aaa3f6175105f935e84753044db
Anna Montserrat <amp1281@gmail.com>
2021-07-19 18:49:24 +0200

    test

---
+++ exercici.3/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <div class="container">
+    <Padre></Padre>
+    </div>
+  </div>
+</template>
+
+<script>
+import Padre from './components/Padre.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Padre
+  },
+}
+</script>
+
+<style lang="scss">
+</style>
---
+++ exercici.3/src/store/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import movies from './../assets/movies.js'
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  
+  state: {
+    movies: movies(), // He importado objeto, con movies() accedo a su property movies
+  },
+
+  getters:{
+
+    //Filtra disponibles
+    availableTrueMovies: state =>{
+       return state.movies.filter(movie => movie.available === true)
+    },
+
+    //Filtra No disponibles
+    availableFalseMovies: state =>{
+      return state.movies.filter(movie => movie.available === false)
+    },
+
+    //Filtra disponibles + Encuentra por título
+    getMoviesAvailable:(state, getters) => (title) => {
+      return getters.availableTrueMovies.filter(movie => movie.title.toLowerCase().indexOf(title.toLowerCase())>-1)
+    },
+
+    //Filtra No disponibles + Encuentra por título
+    getMoviesNotAvailable:(state, getters) => (title) => {
+      return getters.availableFalseMovies.filter(movie => movie.title.toLowerCase().indexOf(title.toLowerCase())>-1)
+    },
+
+
+
+  },
+
+  mutations:{
+    crearArray(state){
+      Array.from(state.movies)
+    },
+  
+  },
+
+})
+
+
+
+
+
+
